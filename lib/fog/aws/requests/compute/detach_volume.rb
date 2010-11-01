51802b2f64f0d5b5e6d04826522a37e4c59a94a0
joewilliams <joe@der-dieb.local>
2010-11-01 12:21:36 -0700

    simple addition of forcing a volume to detach

--- lib/fog/aws/requests/compute/detach_volume.rb
+++ lib/fog/aws/requests/compute/detach_volume.rb
@@ -31,6 +31,11 @@ module Fog
             :parser     => Fog::Parsers::AWS::Compute::DetachVolume.new
           }.merge!(options))
         end
+        
+        def force_detach_volume(volume_id, options = {})
+          options.store('Force', true)
+          detach_volume(volume_id, options)
+        end
 
       end
 
